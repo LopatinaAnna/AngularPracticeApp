957a114bfc49355ec377f52162ac121b14a620a9
Anna Lopatina <an.lopatina050@gmail.com>
2022-06-02 18:40:47 +0300

    cleanup

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,26 +1,8 @@
 import { Component } from '@angular/core';
-import { Service } from '../service';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.css']
 })
-export class TabsComponent {
-  characters = []
-  chosenList = 'all'
-  service: Service
-
-  constructor(service: Service) {
-    this.service = service
-  }
-
-  onChoose(side){
-    this.chosenList = side
-  }
-
-  getCharacters(){
-    this.characters = this.service.getCharacters(this.chosenList)
-    return this.characters
-  }
-}
+export class TabsComponent {}
